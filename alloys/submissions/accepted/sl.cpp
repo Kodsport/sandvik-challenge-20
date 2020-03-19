116f6808798962604694582953c285349c0d7631
Simon Lindholm <simon.lindholm10@gmail.com>
2020-03-19 23:46:03 +0100

    alloys: another solution

---
+++ alloys/submissions/accepted/sl.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, from, to) for (int i = from; i < (to); ++i)
+#define trav(a, x) for (auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+	cin.sync_with_stdio(false);
+	cin.exceptions(cin.failbit);
+	double C;
+	cin >> C;
+	C = min(C, 1.0);
+	double ab = C / 2;
+	cout << setprecision(20) << fixed << ab*ab << endl;
+	exit(0);
+}
