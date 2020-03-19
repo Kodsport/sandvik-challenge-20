79e7c7e0718b83baa00e9d621634ebe43891065e
Johan Sannemo <js@vardinnovation.se>
2020-03-19 23:42:55 +0100

    Solution and statement fix manufacturing

---
+++ sequentialmanufacturing/submissions/accepted/js.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+	cin.sync_with_stdio(0); cin.tie(0);
+	cin.exceptions(cin.failbit);
+    int N, M;
+    cin >> N >> M;
+    int ts = 0, mt = 0;
+    rep(i,0,N) {
+        int t;
+        cin >> t;
+        ts += t;
+        mt = max(t, mt);
+    }
+    cout << ts + (M - 1) * mt << endl;
+}
