c8def860144eb1bf2d1808c39e2c90e30fda8532
Johan Sannemo <js@vardinnovation.se>
2020-03-20 09:54:40 +0100

    one more case, a WA submission

--- witchdance/data/circleclose.py
+++ witchdance/data/circleclose.py
@@ -4,7 +4,7 @@ import random
 N = 200000
 print(N)
 
-D = 1.0 + 1e-6 + 1e-7
+D = 2 * (1.0 + 1e-6 + 1e-7)
 theta = 2 * math.pi / N
 # D = 2R sin(theta/2)
 # D / sin(theta/2) = 2R
@@ -14,5 +14,12 @@ R = D / (2 * math.sin(theta / 2))
 for k in range(N):
     x = math.cos(2 * math.pi / N * k) * R
     y = math.sin(2 * math.pi / N * k) * R
-    r = math.pi / N * k
-    print(x, y, r)
+
+    ox = math.cos(2 * math.pi / N * (k + (-1 if k%2 else 1))) * R
+    oy = math.sin(2 * math.pi / N * (k + (-1 if k%2 else 1))) * R
+    
+    dx = ox - x
+    dy = oy - y
+    ang = 2*math.pi-math.atan2(dy, dx)
+
+    print(x, y, math.fmod(ang+2*math.pi, 2*math.pi))
