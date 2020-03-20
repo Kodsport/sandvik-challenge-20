e6a3a350ca6213c445949479c39d52ed672568b2
Johan Sannemo <js@vardinnovation.se>
2020-03-20 09:33:26 +0100

    one more case

--- witchdance/data/group.py
+++ witchdance/data/group.py
@@ -5,7 +5,7 @@ N = 200000
 print(N)
 
 for k in range(N):
-    x = (random.random() - 0.5) * 2 * 10
-    y = (random.random() - 0.5) * 2 * 10
+    x = (random.random() - 0.5) * 2
+    y = (random.random() - 0.5) * 2
     r = random.random() * 2 * math.pi
     print(x, y, r)
